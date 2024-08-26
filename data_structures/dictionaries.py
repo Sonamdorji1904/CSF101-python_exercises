3d83147773ff828e4ea0272f73f83f2268b06a3c
Sonamdorji1904 <02240362.cst@rub.edu.bt>
2024-08-26 16:47:14 +0600

    Python Exercises

---
+++ data_structures/dictionaries.py
@@ -0,0 +1,21 @@
+name = "Sonam Dorji"
+age = 18
+height = 1.71
+is_student = True
+
+person_info = {
+    "name":name,
+    "age": age,
+    "height": height,
+    "is_student": is_student
+}
+print(person_info)
+
+person_info["favorite_color"] = "sky blue"
+print(person_info)
+
+try:
+    print(person_info["weight"])
+except KeyError as e:
+    print(f"Error: {e}")
+
---
+++ data_structures/lists.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+fruits.append("date")
+print(fruits)
+
+second_fruits = fruits[1]
+print(second_fruits)
+
