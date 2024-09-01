6a7db628f0e44b3b5bb4a17a2fccfe09d425742c
Sonamdorji1904 <02240362.cst@rub.edu.bt>
2024-09-02 00:00:20 +0600

    Python Exercises

--- challenge-ex/ex-ch1.py
+++ challenge-ex/ex-ch1.py
@@ -1,26 +1,41 @@
-with open('fruit_transactions.txt', 'r') as file:
+with open('./fruit_tansactions.txt', 'r') as file:
     data = file.readlines()
 
 length = len(data)
-print(f"The length of data is {length}")
+print(f'the length of data is {length}')
 
-first_line = data[0]
-print(f"The first line is: {first_line}")
+first_line = data(0)
+print(f'the first line is: {first_line}')
 
-line1_dictionary = {
+all_dict_data = []
+for line in data:
+
+    # new v = []
+    line_dictionary = {
     'name': 'test',
     'action': 'test',
     'quantity': 'adsf',
     'item': '0',
     'price': '0'
-}
+    }
+    cleaned_first_line = first_line.replace('\n', '')
+    splitted_line = cleaned_first_line.split(',')
+    print(splitted_line)
+    
+    line_dictionary['name'] = splitted_line[0]
+    line_dictionary['action'] = splitted_line[1]
+    line_dictionary['quantity'] = splitted_line[2]
+    line_dictionary['item'] = splitted_line[3]
+    line_dictionary['price'] = splitted_line[4]
+    all_dict_data.append(line_dictionary)
 
-cleaned_first_line = first_line.replace('\n','')
-splitted_line = cleaned_first_line.split(',')
-print(splitted_line)
+    # print sum(v)
 
-line1_dictionary['name'] = splitted_line[0]
-line1_dictionary['action'] = splitted_line[1]
-line1_dictionary['quantity'] = splitted_line[2]
-line1_dictionary['item'] = splitted_line[3]
-line1_dictionary['price'] = splitted_line[4]
+    # counter variable app = 0
+
+
+
+total_sales = 0
+for item in all_dict_data:
+    sale = item
+
